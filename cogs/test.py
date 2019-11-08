3ca9ea818f6a27871c1bbe67f8a7e4726faa1a8a
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2019-11-08 18:13:44 +0800

    params for bot to access cog

--- cogs/test.py
+++ cogs/test.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
 
+client = discord.Client()
+
+class Test_room(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Test Room is in use')
+        
+ def setup(client):
+    client.add_cog(Test_room(client))
