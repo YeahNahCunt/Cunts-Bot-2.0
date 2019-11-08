f9d5499b542ef3dec5589f86d58cc008ba984356
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2019-11-08 18:05:59 +0800

    Whoops

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-lient = commands.Bot(command_prefix = '|') #holder prefix change later
+client = commands.Bot(command_prefix = '|') #holder prefix change later
 
 @client.event
 async def on_ready():
